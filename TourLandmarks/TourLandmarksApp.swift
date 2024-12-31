0475e6c150e8c5f4b30c60bcc4a11b1b913bfe62
89Explorer <leral3@naver.com>
2024-12-31 11:44:09 +0900

    Initial Commit

---
+++ TourLandmarks/TourLandmarksApp.swift
@@ -0,0 +1,20 @@
+//
+//  TourLandmarksApp.swift
+//  TourLandmarks
+//
+//  Created by 권정근 on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct TourLandmarksApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ TourLandmarksTests/TourLandmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  TourLandmarksTests.swift
+//  TourLandmarksTests
+//
+//  Created by 권정근 on 12/31/24.
+//
+
+import Testing
+@testable import TourLandmarks
+
+struct TourLandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
